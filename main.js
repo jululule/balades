410b9b97b6a148572a16d10b87850f37964e43ec
Juliane Christen <Juliane Christen>
2017-11-25 21:16:13 +0100

    POC initial

---
+++ main.js
@@ -0,0 +1,6 @@
+var mymap = L.map('mapid').setView([47.266, -1.469], 13);
+
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
